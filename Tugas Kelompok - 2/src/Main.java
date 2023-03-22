3d4386d174ce16e57c68692e079ddd8cc8bed675
Ashari Muhammad Hisbulloh <ashari@stockbit.com>
2023-03-22 15:24:58 +0700

    Add skeleton file for tugas 2

---
+++ Tugas Kelompok - 2/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args){
+
+        printl("Hehehe");
+    }
+
+    public static void print(String param) {
+
+        System.out.print(param);
+    }
+
+    public static void printl(String param) {
+
+        System.out.println(param);
+    }
+
+    public static void printf(String param) {
+
+        System.out.printf(param);
+    }
+}
