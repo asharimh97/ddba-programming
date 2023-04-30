4f8195c572acdfb5c6820c265abca9197f280caf
Ashari Muhammad Hisbulloh <ashari@stockbit.com>
2023-04-30 19:12:49 +0700

    Add data dictionary

--- Tugas Kelompok - 3/src/Main.java
+++ Tugas Kelompok - 3/src/Main.java
@@ -1,5 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static BufferedReader buffer;
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        String word = "letter";
+        boolean isFound = false;
+
+        initDictionary();
+    }
+
+    public static void initDictionary() throws IOException {
+        try {
+            String path = new File(".").getCanonicalPath();
+            String dictionaryPath = path + "/src/data.txt";
+            File f = new File(dictionaryPath);
+
+            BufferedReader b = new BufferedReader(new FileReader(f));
+            buffer = b;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
