ca0e52e482ba5950ce4ef88ceee627839caab2b4
dhani87 <33487333+dhani87@users.noreply.github.com>
2023-05-05 09:10:02 +0700

    update no 1

--- Tugas Kelompok - 4/src/Main.java
+++ Tugas Kelompok - 4/src/Main.java
@@ -1,12 +1,60 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int[] sampleArray = { 5, 1, 4, 2, 8 };
+import java.util.Scanner;
 
-        bubbleSortAscending(sampleArray);
+public class Main {
+        static int[] data = new int[5];
+        static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        int choice;
+        do {
+            System.out.println(""
+                        + "\nSelamat Datang di Program Simulasi"+"\n"
+                        + "Menu"+"\n"
+                        + "1. Random Data"+"\n"
+                        + "2. Simulasi Bubble Sort - Ascending"+"\n"
+                        + "3. Simulasi Selection Sort - Ascending"+"\n"
+                        + "4. Simulasi Bubble Sort - Descending"+"\n"
+                        + "5. Simulasi Selection Sort - Descending"+"\n"
+                        + "6. Keluar"+"\n"
+                        + "Masukkan Pilihan Anda."+"\n");
+                        
+        choice = input.nextInt();
+ 
+        
+        switch (choice) {
+            case 1:
+                randomData();
+                break;
+            case 2:
+                int lowerBound = data[0];
+                int upperBound = data[data.length - 1];
+                bubbleSortAscending(data, lowerBound, upperBound);
+                break;
+            //Lanjutkan
+            case 6:
+                System.out.println("Terimakasih telah menggunakan aplikasi ini");
+                break;
+            } 
+        } while (choice != 6);
+    }
+    
+    public static void randomData() {
+            System.out.print("Masukkan batas bawah : ");
+                int lowerBound = input.nextInt();
+                    System.out.print("Masukkan batas atas : ");
+                int upperBound = input.nextInt();
+
+            for (int i = 0; i < data.length; i++) {
+                    data[i] = (int) (Math.random() * (upperBound - lowerBound + 1)) + lowerBound;
+                }
+
+            System.out.println("Data yang dihasilkan :");
+                for (int i = 0; i < data.length; i++) {
+                    System.out.print(data[i] + " ");
+                    }
+                System.out.println();
     }
 
-    public static void bubbleSortAscending(int[] arr) {
+    public static void bubbleSortAscending(int[] arr, int lowerBound, int upperBound) {
         int len = arr.length;
 
         for (int i = 0; i < len - 1; i++) {
